062aefbfd06f85959774913aa16f142e0b91ad70
Lauren Erxleben <lauren.n.erxleben@gmail.com>
2022-06-29 21:15:42 -0600

    finished code

---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+// src="https://via.placeholder.com/215", about
+function About({ image = "https://via.placeholder.com/215", about }) {
+    return (
+        <aside>
+            <img src={image} alt="blog logo" />
+            <p>{about}</p>
+        </aside>
+    )
+
+}
+
+
+export default About;
---
+++ src/components/Article.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+function emojiFromMinutes(minutes) {
+  let emojis = "";
+
+  if (minutes < 30) {
+    for (let i = 0; i < minutes; i += 5) {
+      emojis += "☕️";
+    }
+  } else {
+    for (let i = 0; i < minutes; i += 10) {
+      emojis += "🍱";
+    }
+  }
+  return emojis
+}
+
+function Article({ minutes, title, date = "January 1, 1970", preview }) {
+  return (
+    <article>
+      <h3>{title}</h3>
+      <small>
+        {date} | {emojiFromMinutes(minutes)} {minutes} min read
+      </small>
+      <p>{preview}</p>
+    </article>
+  );
+}
+
+export default Article;
